f405ab47e71b605397a54953aa7651ceaaa8fa39
Andrew <yau7237@gmail.com>
2022-10-04 22:00:39 +0500

    CaruselDone

--- client/src/components/singleCard/SingleCard.jsx
+++ client/src/components/singleCard/SingleCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SingleCard.css';
+import './myNewGalleryStyles.css';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,6 +9,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
 import StarIcon from '@mui/icons-material/Star';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 const SingleCard = () => {
   return (
@@ -19,6 +22,7 @@ const SingleCard = () => {
           position: 'absolute',
           top: '15px',
           right: '20px',
+          zIndex:"100"
         }}
       >
         <FavoriteTwoToneIcon sx={{ color: 'white' }} />
@@ -33,14 +37,32 @@ const SingleCard = () => {
           boxShadow: 0,
         }}
       >
-        <CardMedia
-          sx={{ borderRadius: '5%' }}
-          component="img"
-          height="290"
-          width="305"
-          image="https://trizio.ru/img-srv01/052017/img_post/top_348.jpg"
-          alt="green iguana"
-        />
+        <Carousel showStatus={false} showArrows={true}  >
+          <CardMedia
+            sx={{ borderRadius: '5%' }}
+            component="img"
+            height="290"
+            width="305"
+            image="https://trizio.ru/img-srv01/052017/img_post/top_348.jpg"
+            alt="green iguana"
+          />
+          <CardMedia
+            sx={{ borderRadius: '5%' }}
+            component="img"
+            height="290"
+            width="305"
+            image="https://houses.ru/upload/medialibrary/7e4/kk04.jpg"
+            alt="green iguana"
+          />
+          <CardMedia
+            sx={{ borderRadius: '5%' }}
+            component="img"
+            height="290"
+            width="305"
+            image="https://i.pinimg.com/originals/c3/a0/f0/c3a0f0010e46a525c3e3709c97f4a232.png"
+            alt="green iguana"
+          />
+        </Carousel>
         <CardContent sx={{ textAlign: 'left', padding: '10px 0 0 0' }}>
           <div className="container__rate">
             <Typography
