667057c72045ed2829e405f790e2a497dc5002d3
Evgeniy3234 <evgeniy3234@gmail.com>
2022-10-05 15:17:28 +0300

    no comments

--- client/src/components/SingleFlat/SingleFlat.jsx
+++ client/src/components/SingleFlat/SingleFlat.jsx
@@ -7,14 +7,11 @@ import { addCard, addPhoto} from '../../RTKSlice/rtkslice'
 const Flats = () => {
   const [ card, setCard ]= useState([]);
  
-
-
   useEffect(() => {
     axios.get(`http://localhost:3001/singleFlat/${}`, {withCredentials: true})
     .then((res)=>{
       setCard(res.data.flat)
       setPhoto(res.data.photoflat)
-      // console.log('res.data', res.data);
     })
 
   },[])
--- server/routes/card.route.js
+++ server/routes/card.route.js
@@ -1,19 +1,13 @@
 const route = require('express').Router();
-
 const { Flat } = require('../db/models');
 
 route.post('/', async (req, res) => {
   try {
-    console.log('req.body',req.body);
     const flat = await Flat.findAndCountAll({ raw: true, limit: 10, offset: req.body.currentPage });
-    // const photoflat = await PhotoFlat.findAll({ raw: true });
-    // console.log('!!!!!!!!!!!', flat);
     res.json({ flat });
   } catch (error) {
     console.error('Error', error);
   }
 });
-
-
 
 module.exports = route;
