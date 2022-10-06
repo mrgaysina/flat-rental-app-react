e7e7c53b3461322ce6b016e4f843d25d2eaafa00
Андрей  Исупов <yau7237@gmail.com>
2022-10-06 13:20:35 +0500

    Loader

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -3,60 +3,65 @@ import SingleCard from '../../components/singleCard/SingleCard';
 import Box from '@mui/material/Box';
 import './Cards.css';
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllCard } from '../../RTKSlice/rtkslice'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCard } from '../../RTKSlice/rtkslice';
+import Loader from '../loader/Loader';
 
 const Cards = () => {
-
   const [currentPage, setCurrentPage] = useState(0);
   const [fetching, setFetching] = useState(true);
-  const card = useSelector(store => store.toolkit.card)
-  const dispatch = useDispatch()
+  const [isFetching, setIsFetching] = useState(false);
+  const card = useSelector((store) => store.toolkit.card);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if(fetching){
-
-      axios.post('http://localhost:3001/allFlat', {currentPage}, {withCredentials: true})
-      .then((res)=>{
-        console.log('res.data',res.data);
-        dispatch(getAllCard([...card,...res.data.flat.rows]));
-        setCurrentPage((prevState) => prevState + 10);
-      })
-      .finally(()=> setFetching(false))
+    if (fetching) {
+      axios
+        .post(
+          'http://localhost:3001/allFlat',
+          { currentPage },
+          { withCredentials: true },
+          setIsFetching(true)
+        )
+        .then((res) => {
+          console.log('res.data', res.data);
+          dispatch(getAllCard([...card, ...res.data.flat.rows]));
+          setCurrentPage((prevState) => prevState + 10);
+        })
+        .finally(() => {
+          setFetching(false);
+          setIsFetching(false);
+        });
     }
-
-  },[fetching])
+  }, [fetching]);
 
   useEffect(() => {
-          document.addEventListener('scroll', scrollHandler);
-          return function () {
-             document.removeEventListener('scroll', scrollHandler);
-          };
-       }, []);
-    
-       const scrollHandler = (e) => {
-          if (
-             e.target.documentElement.scrollHeight -
-                (e.target.documentElement.scrollTop + window.innerHeight) <
-             100
-          ) {
-             setFetching(true);
-          }
-       };
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
+    }
+  };
 
   return (
-    <Box
-    className="wrapper"
-    marginTop="20px"
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    >
-      {
-        card.map((el) =>  <SingleCard key={el.id} el={el}  />)
-      }
-   
-    </Box>
+    <div className="wrapper">
+      {card.map((el) => (
+        <SingleCard
+          key={el.id}
+          el={el}
+        />
+      ))}
+      {isFetching && card.map((el) => <Loader />)}
+    </div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Home.css';
 import Cards from '../components/cards/Cards';
 import Category from '../components/category/Category';
+
 
 const Home = () => {
   return (
     <div>
       <Category />
-      <Cards />
+      <div className='cards__box'>
+        <Cards />
+      </div>
     </div>
   );
 };
